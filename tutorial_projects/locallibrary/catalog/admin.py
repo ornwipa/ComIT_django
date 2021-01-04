c4e91c2692d2dbc0e4c375a367767ca7596f59b1
ornwipa <fahornwipa@gmail.com>
2021-01-04 16:56:18 -0600

    fix bugs: inlines list/tuple, callable attribute

--- tutorial_projects/locallibrary/catalog/admin.py
+++ tutorial_projects/locallibrary/catalog/admin.py
@@ -18,7 +18,7 @@ class BooksInline(admin.TabularInline):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-    inlines = Book
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -38,7 +38,8 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    # list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, {
