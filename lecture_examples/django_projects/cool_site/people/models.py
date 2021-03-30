54d7eafcafe3a4bd23b855133a86a52c9fdabb95
ornwipa <fahornwipa@gmail.com>
2021-03-30 10:14:50 -0600

    (missed) lecture: database, django models, foreign key

--- lecture_examples/django_projects/cool_site/people/models.py
+++ lecture_examples/django_projects/cool_site/people/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
-# Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=6)
 
 class Person(models.Model):
     first_name = models.TextField(max_length=50)
@@ -8,4 +12,5 @@ class Person(models.Model):
     email = models.EmailField()
     # profile_pic =  models.ImageField(blank=True)
     profile_pic =  models.ImageField(upload_to="images/")
+    address = models.ForeignKey(Address, on_delete=SET_NULL, null=True)
     
